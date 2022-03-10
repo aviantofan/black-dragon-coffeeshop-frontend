81c499af3184fed19c9250ad6c0917ce6aa12a2f
rintosaputro <saputrorinto@gmail.com>
2022-03-10 21:19:02 +0700

    feat: init

---
+++ src/pages/ProductList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function ProductList() {
+  return (
+    <div className='product-list'>
+      <main className='row'>
+        <aside className='col-12 co-lg-4 container'>
+          <h3 className='text-primary'>Promo Today</h3>
+          <p>Coupons will be updated every weeks. Check them out! </p>
+        </aside>
+        <section className='col-12 col-lg-8'>
+
+        </section>
+      </main>
+    </div>
+  )
+}
