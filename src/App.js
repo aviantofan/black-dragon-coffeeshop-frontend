f868a2c434e7fe2d48abac621fae1e161756a548
Asher Azriel Ginting <asherginting3@gmail.com>
2022-03-10 22:57:51 +0700

    feat: add route admin home

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 // import { Button } from 'react-bootstrap';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import HomeAdmin from './pages/HomeAdmin';
 
 export const App = () => {
   return (
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home/>}/>
+      <Route path='/admin' element={<HomeAdmin/>}/>
     </Routes>
     </BrowserRouter>
   );
