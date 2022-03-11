105e5ad31e3160f031603222f795e972daa4d1ce
Asher Azriel Ginting <asherginting3@gmail.com>
2022-03-11 16:00:25 +0700

    feat: add custom scss

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle';
 import '../src/assets/css/profilePage.css';
 import '../src/assets/css/style.css';
+import '../src/assets/scss/custom.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
