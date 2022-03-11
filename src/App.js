d84b75258558dc672ed6c8f5be10e606d4c6a015
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-03-11 08:19:04 +0700

    feat : init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import InputProduct from './pages/InputProduct';
 
-function App() {
+function App () {
   return (
     <div className="App">
-      <header className="App-header">
+      <InputProduct></InputProduct>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +19,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
