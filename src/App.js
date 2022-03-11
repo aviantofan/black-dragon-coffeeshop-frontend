9cf6524ade80de5ec1ef23605f45520717d3e3fc
eexxprd <aviantofan@gmail.com>
2022-03-11 09:28:32 +0700

    feat: add profilePage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import ForgotPage from './pages/forgotPage';
+import ProfilePage from './pages/profilePage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element ={<ForgotPage/>}/>
+        <Route path='/' element={<ProfilePage/>}/>
+        <Route path='forgotPage' element ={<ForgotPage/>}/>
       </Routes>
   </BrowserRouter>
   );
