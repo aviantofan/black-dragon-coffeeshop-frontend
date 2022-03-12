56a72e7a7346e8f59a95ce20cd60d422c7cce9c4
rintosaputro <saputrorinto@gmail.com>
2022-03-12 12:14:46 +0700

    feat: add histories content

--- src/pages/HistoriesList.js
+++ src/pages/HistoriesList.js
@@ -1,7 +1,35 @@
 // import React from 'react'
+import '../assets/scss/historieslist.scss';
+import Layout from '../layouts/Layout';
+import imgProduct from '../assets/images/product.jpg';
 
 export default function HistoriesList () {
   return (
-    <div>HistoriesList</div>
+    <Layout>
+      <section className='histories-list-page'>
+        <div className='opacity'>
+          <div className='container'>
+            <div className='text-center text-white head-text'>
+              <h1>Let`s see what you have bought!</h1>
+              <span>Long press to delete item</span>
+            </div>
+            <div className='row g-4 data-histories'>
+              {[...Array(15)].map((data, index) => {
+                return <div key={index} className='col-12 col-md-6 col-lg-4'>
+                <div className='card d-flex flex-row justify-content-between p-3 p-xl-2 p-xxl-4'>
+                  <div className='img-product' style={{ backgroundImage: `url(${imgProduct})` }}></div>
+                  <div className='history-text'>
+                    <h4>Veggie tomato mix</h4>
+                    <div>IDR 34.000</div>
+                    <div>Delivered</div>
+                  </div>
+                </div>
+              </div>;
+              })}
+            </div>
+          </div>
+        </div>
+      </section>
+    </Layout>
   );
 }
