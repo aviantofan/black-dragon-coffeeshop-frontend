d4324f59addb1956a392509f4c8768197890ceaf
Asher Azriel Ginting <asherginting3@gmail.com>
2022-03-12 18:00:38 +0700

    feat: add link to images, signup, forgot

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,6 +5,7 @@ import Button from '../components/Button';
 import Input from '../components/Input';
 import google from '../assets/images/google.png';
 import Layout from '../layouts/Layout';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
@@ -16,9 +17,13 @@ const Login = () => {
             <div className='col-12 col-lg-6 px-5 top-section'>
               <div className='my-5 d-flex flex-row justify-content-between'>
                 <div className='logo'>
+                  <Link to='/'>
                   <img src={logo} alt='logo' />
+                  </Link>
                 </div>
+                <Link to='/signup'>
                 <Button className='px-5'>Sign Up</Button>
+                </Link>
               </div>
               <div className='text-center'>
                 <h2 className='text-secondary text-center my-5'>Login</h2>
@@ -26,7 +31,9 @@ const Login = () => {
                   <Input label='email adress' />
                   <Input label='password' cls='mt-5' />
                   <div className='text-start input-section my-3'>
-                    <a href='#'>Forgot Password?</a>
+                    <Link to='/forgot'>
+                    <a>Forgot Password?</a>
+                    </Link>
                   </div>
                   <button className='btn btn-primary btn-full my-3'>Login</button>
                   <button className='btn btn-secondary btn-full'><img src={google} alt='google' className='pe-1' /> Login With Google</button>
