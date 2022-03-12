75a7c54047dee5834147773f93ca83e3e90eb290
rintosaputro <saputrorinto@gmail.com>
2022-03-12 14:43:03 +0700

    feat: add delete button

--- src/pages/HistoriesList.js
+++ src/pages/HistoriesList.js
@@ -1,9 +1,13 @@
-// import React from 'react'
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
 import '../assets/scss/historieslist.scss';
 import Layout from '../layouts/Layout';
 import imgProduct from '../assets/images/product.jpg';
+import { FiTrash2 } from 'react-icons/fi';
 
 export default function HistoriesList () {
+  const [showDelete, setShowDelete] = useState();
+
   return (
     <Layout>
       <section className='histories-list-page'>
@@ -15,14 +19,18 @@ export default function HistoriesList () {
             </div>
             <div className='row g-4 data-histories'>
               {[...Array(15)].map((data, index) => {
-                return <div key={index} className='col-12 col-md-6 col-lg-4'>
-                <div className='card d-flex flex-row justify-content-between p-3 p-xl-2 p-xxl-4'>
+                return <div key={index} className='col-12 col-md-6 col-lg-4 position-relative'>
+                <div style={{ cursor: 'pointer' }} className='card d-flex flex-row  p-3 p-xl-2 p-xxl-4'>
                   <div className='img-product' style={{ backgroundImage: `url(${imgProduct})` }}></div>
-                  <div className='history-text'>
+                  <div className='history-text ps-3'>
                     <h4>Veggie tomato mix</h4>
                     <div>IDR 34.000</div>
                     <div>Delivered</div>
                   </div>
+                </div>
+                <div id={`number${index}`} className={`position-absolute delete-option d-${!showDelete ? 'none' : 'block'}`}>
+                  <button className='btn-delete bg-secondary'><FiTrash2/></button>
+                  <button className='btn-delete bg-primary ms-2 fw-bold'>x</button>
                 </div>
               </div>;
               })}
