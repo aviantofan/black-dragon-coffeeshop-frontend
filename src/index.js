06dae8c22cb6a3f6ddf17f2255cc676f13b232f5
rintosaputro <saputrorinto@gmail.com>
2022-03-14 00:32:44 +0700

    fix: fetch data product

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './assets/scss/custom.scss';
 import './index.css';
-
+import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './redux/store';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import { productList } from './products';
+
+const rootReducer = combineReducers({
+  productList
+});
+
+export default rootReducer;
